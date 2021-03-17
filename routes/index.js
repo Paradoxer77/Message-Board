097190924f0673f8f9fb3803bc2f266349a93179
Paradoxer77 <chapagainsakshyam@gmail.com>
2021-03-17 20:12:10 +0545

    Added message to views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const messages = [
+  {
+    text: "Hi There",
+    user: "Amanda",
+    added: new Date(),
+  },
+  {
+    text: "Hello World",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message Board", messages });
 });
 
 module.exports = router;
