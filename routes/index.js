7f41fce05f99ca82565f9bb61e6da7d81a9e5187
Paradoxer77 <chapagainsakshyam@gmail.com>
2021-06-07 19:58:10 +0545

    Update index router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi, Okay!",
+    user: "Jack Prasad",
+    date: new Date(),
+  },
+  {
+    text: "Hello",
+    user: "No",
+    date: new Date(),
+  },
+  {
+    text: "Imma end this man's whole career",
+    user: "??",
+    date: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { messages });
 });
 
 module.exports = router;
