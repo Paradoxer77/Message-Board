a9088a1e856167791fa58eee2b2faf1b9e832f5b
Paradoxer77 <chapagainsakshyam@gmail.com>
2021-06-07 20:38:01 +0545

    Added form

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,20 @@ const messages = [
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { messages });
+  res.render("index", { title: "Mini Messageboard", messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    date: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
